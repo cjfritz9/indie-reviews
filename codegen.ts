34c4dde637317c2716d0e5b9c4fd8eb8b6cb0266
CJ <cjfritz9@gmail.com>
2023-10-04 20:19:15 -0600

    production check

--- codegen.ts
+++ codegen.ts
@@ -2,7 +2,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: 'http://localhost:1337/graphql',
+  schema: `${process.env.CMS_URL}/graphql`,
   documents: 'graphql/**/*.graphql',
   generates: {
     'graphql/generated/codegen.ts': {
--- graphql/apollo.ts
+++ graphql/apollo.ts
@@ -1,9 +1,8 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:1337/graphql',
-  cache: new InMemoryCache(),
-  
+  uri: `${process.env.CMS_URL}/graphql`,
+  cache: new InMemoryCache()
 });
 
 export default client;
